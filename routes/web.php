92c240a880232d9e902197399afacea333c25aa9
wojciechdrabczyk <drabczyk@proton.me>
2024-12-11 14:54:05 +0100

    refactor: use Inertia for routing in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,25 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-    ]);
-})->name('home');
+// Static Pages
+Route::inertia('/', 'Welcome', [
+    'canLogin' => Route::has('login'),
+    'canRegister' => Route::has('register'),
+])->name('home');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::inertia('/dashboard', 'Dashboard')->middleware(['auth', 'verified'])->name('dashboard');
+Route::inertia('/contact', 'Contact')->name('contact');
+Route::inertia('/about', 'About')->name('about');
+Route::inertia('/menu', 'Menu')->name('menu');
 
-Route::get('/contact', function () {
-    return Inertia::render('Contact');
-})->name('contact');
-
+// Authenticated Profile Routes
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('/profile', 'edit')->name('profile.edit');
+        Route::patch('/profile', 'update')->name('profile.update');
+        Route::delete('/profile', 'destroy')->name('profile.destroy');
 
+    });
+});
+// Include auth routes
 require __DIR__ . '/auth.php';
