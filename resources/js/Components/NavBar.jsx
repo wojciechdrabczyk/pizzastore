893f406623d41983f38de5c1384d1f47ce44a806
wojciechdrabczyk <drabczyk@proton.me>
2024-12-16 14:48:39 +0100

    refactor: minor changes

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -1,24 +1,24 @@
-import { Link, usePage } from "@inertiajs/react";
+import {Link, usePage} from "@inertiajs/react";
 import ShoppingCart from "@/Components/Icons/ShoppingCart.jsx";
 
 export default function NavBar() {
-    const { auth } = usePage().props;
+    const {auth} = usePage().props;
 
     const getNavLinkClasses = (routeName) => `rounded-md px-3 py-2 ring-1 ring-transparent transition
         hover:text-[#FF5733] focus:text-[#FF5733] focus:outline-none focus-visible:ring-[#FF2D20]
         ${route().current(routeName) ? 'text-[#FF5733] font-bold' : 'text-white'}`;
 
     return (
-        <nav className="flex items-center justify-between bg-gray-400 p-4">
+        <nav className="flex items-center justify-between bg-gray-400 p-4 w-full">
             <div className="text-xl font-bold text-white">
                 PizzaMia
             </div>
 
             <div className="flex-grow flex justify-center space-x-6">
-                {[{ name: 'Home', routeName: 'home' },
-                    { name: 'Menu', routeName: 'menu' },
-                    { name: 'Contact Us', routeName: 'contact' },
-                    { name: 'About Us', routeName: 'about' },
+                {[{name: 'Home', routeName: 'home'},
+                    {name: 'Menu', routeName: 'menu'},
+                    {name: 'Contact Us', routeName: 'contact'},
+                    {name: 'About Us', routeName: 'about'},
                 ].map((item) => (
                     <Link
                         key={item.routeName}
@@ -40,11 +40,10 @@ export default function NavBar() {
                         <Link href={route('login')} className={getNavLinkClasses('login')}>
                             Sign In
                         </Link>
-                        {/* ShoppingCart icon placed next to Sign In */}
                         <button className="relative flex items-center text-white hover:text-[#FF5733]">
-                            <ShoppingCart className="h-6 w-6" /> {/* Adjust size here */}
-                            {/* Optional cart item count badge */}
-                            <span className="absolute top-0 right-0 flex h-4 w-4 items-center justify-center rounded-full bg-red-600 text-xs text-white">
+                            <ShoppingCart className="h-6 w-6"/>
+                            <span
+                                className="absolute top-0 right-0 flex h-4 w-4 items-center justify-center rounded-full bg-red-600 text-xs text-white">
                                 3
                             </span>
                         </button>
